6c2e8b29f456cbefdb241d3b4d07174aad18637f
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-11-09 12:40:23 -0300

    Document schema

--- schema/time_registry.js
+++ schema/time_registry.js
@@ -1,23 +1,46 @@
 import { gql } from 'apollo-server';
 
 module.exports = gql`
+
+"""
+TimeRegistry representa uma batida de ponto
+"""
 type TimeRegistry {
     id: ID!
+    "ID do usuário que efetuou a batida de ponto"
     userId: ID!
+    "Usuário que efetuou a batida de ponto"
     user: User!
+    "Data em quem a batida de ponto foi efetuada"
     createdAt: Date!
 }
 
 extend type Subscription {
+    """
+    Evento que é disparado quando uma batida é efetuada.
+    Note que somente administradores podem subscrever a este evento
+    """
     timeRegistryCreated: TimeRegistry @isAuthenticated @hasRole(role: "ADMIN")
 }
 
 extend type Query {
+    """
+    Query que retorna todas as batidades de pontos de todos usuários.
+    Note que somente administradores podem executar essa query
+    """
     timeRegistries: [TimeRegistry!] @isAuthenticated @hasRole(role: "ADMIN")
 }
 
 extend type Mutation {
-    createTimeRegistry: TimeRegistry!
+    """
+    Efetua uma batida de ponto para o usuário corrente.
+    Note que somente usuários authenticados podem executar essa mutation
+    """
+    createTimeRegistry: TimeRegistry! @isAuthenticated
+    """
+    Apaga uma batida de ponto de qualquer usuário.
+    Note que somente administradores podem executar essa mutation
+    """
     deleteTimeRegistry(id: ID!): Boolean @isAuthenticated @hasRole(role: "ADMIN")
 }
 `
--- schema/user.js
+++ schema/user.js
@@ -1,30 +1,49 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
+"""
+Tipos de papéis de usuários
+"""
 enum Role {
+  "Usuário comum"
   USER
+  "Administrador"
   ADMIN
 }
 
+"User representa um usuário"
 type User {
   id: ID!
+  "Nome do usuário"
   name: String!
+  "E-mail do usuário"
   email: String!
+  "Senha de acesso do usuário"
   password: String!
+  "Papel do usuário"
   role: Role
+  "Batidas de pontos do usuário"
   timeRegistries: [TimeRegistry!]!
 }
 
 extend type Query {
+  """
+  Query que retorna todos os usuários
+  Note que somente administradores podem executar essa query
+  """
   users: [User!] @isAuthenticated @hasRole(role: "ADMIN")
+  "Retorna o usuário corrente que está autenticado"
   currentUser: User @isAuthenticated
 }
 
 extend type Mutation {
+  "Mutation para registro de novos usuários"
   signUp(name: String!, email: String!, password: String!): Token!
+  "Mutation para login de usuários registrados"
   signIn(email: String!, password: String!): Token!
 }
 
+"Token de autenticação"
 type Token {
   token: String!
 }
