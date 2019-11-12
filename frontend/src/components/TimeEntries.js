f4f8d55539a98bacfa717d306e85059854c8e30d
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-11-12 11:55:29 -0300

    frontend: subscribe for events

--- frontend/src/components/TimeEntries.js
+++ frontend/src/components/TimeEntries.js
@@ -10,9 +10,14 @@ import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Chip from '@material-ui/core/Chip';
 import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import { Typography } from '@material-ui/core';
 
 const TIME_ENTRIES = gql`
   query TimeEntries {
@@ -37,7 +42,10 @@ const CREATE_TIME_ENTRY = gql`
   mutation CreateTimeEntry {
     createTimeEntry {
         id,
-        createdAt
+        createdAt,
+        user {
+            name
+        }
     }
   }
 `;
@@ -45,6 +53,18 @@ const CREATE_TIME_ENTRY = gql`
 const DELETE_TIME_ENTRY = gql`
   mutation DeleteTimeEntry($id: ID!) {
     deleteTimeEntry(id: $id)
+  }
+`;
+
+const TIME_ENTRY_CREATED = gql`
+  subscription TimeEntryCreated {
+    timeEntryCreated {
+      id,
+      createdAt,
+      user {
+        name
+      }
+    }
   }
 `;
 
@@ -69,7 +89,7 @@ function TimeEntries() {
 
     const [deleteTimeEntry] = useMutation(DELETE_TIME_ENTRY);
 
-    useQuery(TIME_ENTRIES, {
+    const { updateQuery } = useQuery(TIME_ENTRIES, {
         onCompleted: (data) => {
             setTimeEntries(data.timeEntries);
         }
@@ -88,32 +108,53 @@ function TimeEntries() {
 
     return (
         <>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell align="center">Data</TableCell>
-                        {admin ? (<TableCell align="center">Dono</TableCell>) : (<></>)}
-                        <TableCell align="center">Tipo</TableCell>
-                        <TableCell align="center"></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {timeEntries.map((entry, index) => (
-                        <TableRow key={entry.id}>
-                            <TableCell component="th" scope="row">{entry.id}</TableCell>
-                            <TableCell align="center"><Timestamp date={entry.createdAt} /></TableCell>
-                            {admin ? (<TableCell align="center">{entry.user.name}</TableCell>) : (<></>)}
-                            <TableCell align="center">
-                                {index % 2 === 0 ? (<Chip label="Entrada" color="primary" />) : (<Chip label="Saída" color="secondary" />)}
-                            </TableCell>
-                            <TableCell align="center">
-                                {admin ? <IconButton onClick={() => deleteTimeEntryHelper(entry.id)}><DeleteIcon /></IconButton> : ''}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">Sistema de Ponto</Typography>
+                </Toolbar>
+            </AppBar>
+
+            {timeEntries.length === 0 ? (
+                <Container component="main" maxWidth="md">
+                    <Grid container
+                        spacing={0}
+                        alignItems="center"
+                        justify="center"
+                        style={{ minHeight: '80vh' }}>
+                        <div>
+                            <Typography variant="h3" align="center">Não existem batidas registradas</Typography>
+                            <Typography variant="subtitle1" align="center">Clique no botão do canto inferior esquerdo para registrar uma batida</Typography>
+                        </div>
+                    </Grid>
+                </Container>
+            ) : (
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell align="center">Data</TableCell>
+                                {admin ? (<TableCell align="center">Dono</TableCell>) : (<></>)}
+                                <TableCell align="center">Tipo</TableCell>
+                                <TableCell align="center"></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {timeEntries.map((entry, index) => (
+                                <TableRow key={entry.id}>
+                                    <TableCell component="th" scope="row">{entry.id}</TableCell>
+                                    <TableCell align="center"><Timestamp date={entry.createdAt} /></TableCell>
+                                    {admin ? (<TableCell align="center">{entry.user.name}</TableCell>) : (<></>)}
+                                    <TableCell align="center">
+                                        {index % 2 === 0 ? (<Chip label="Entrada" color="primary" />) : (<Chip label="Saída" color="secondary" />)}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {admin ? <IconButton onClick={() => deleteTimeEntryHelper(entry.id)}><DeleteIcon /></IconButton> : ''}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                )}
             <Fab color="primary" className={classes.fab} onClick={createTimeEntry}>
                 <AddIcon />
             </Fab>
